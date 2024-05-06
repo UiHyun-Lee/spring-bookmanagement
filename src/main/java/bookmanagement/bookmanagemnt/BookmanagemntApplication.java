b7df309de2e4f17c98f304c235dfcbe33ac3b949
uihyunlee <deutschluh@gmail.com>
2024-05-06 17:17:05 +0200

    first commit

---
+++ src/main/java/bookmanagement/bookmanagemnt/BookmanagemntApplication.java
@@ -0,0 +1,13 @@
+package bookmanagement.bookmanagemnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmanagemntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmanagemntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bookmanagement/bookmanagemnt/BookmanagemntApplicationTests.java
@@ -0,0 +1,13 @@
+package bookmanagement.bookmanagemnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagemntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
